1b7d84d6a642f88708f0d95dbcf06b7513a0fd2f
Alex Gonzalez <gonzo_rhcp@hotmail.com>
2018-10-09 10:06:38 +1100

    Improved 'ConvertTo-IdentityObject' (support for PowerShell v2.0).

--- CmdLets/ConvertTo-IdentityObject.psm1
+++ CmdLets/ConvertTo-IdentityObject.psm1
@@ -11,34 +11,60 @@
     }
         
     process {
-        if ($InputObject -is [Security.Principal.SecurityIdentifier]) {
+        $a_name = ""
+        if ($InputObject.GetType() -eq [Security.Principal.SecurityIdentifier]) {
             $sid = $InputObject
         }
-        elseif ($InputObject -is [string]) {
+        elseif ($InputObject.GetType() -eq [string]) {
             if ($InputObject.StartsWith("S-")) {
                 # S-1-5-21-2357950043-1680534590-2315089127-500
-                $sid = New-Object System.Security.Principal.SecurityIdentifier($InputObject)
+                $sid = New-Object System.Security.Principal.SecurityIdentifier($InputObject)
             }
             else {
                 # Administrator
                 $accountName = New-Object System.Security.Principal.NTAccount($InputObject)     
-                $sid = $accountName.Translate([System.Security.Principal.SecurityIdentifier])
+
+                $eap = $ErrorActionPreference 
+                $ErrorActionPreference = "Stop"
+                try {
+                    $sid = $accountName.Translate([System.Security.Principal.SecurityIdentifier])
+                }
+                catch {
+                    $sid = ""
+                    $a_name = $InputObject
+                }
+                $ErrorActionPreference = $eap 
             }
-        }
-        elseif($InputObject -is [byte[]]) {
+        }
+        elseif ($InputObject.GetType() -eq [byte[]]) {
             # @(1, 5, 0, 0, 0, 0, 0, 5, 21, 0, 0, 0, 91, 118, 139, 140, 62, 236, 42, 100, 231, 116, 253, 137, 244, 1, 0, 0)
-            $sid = New-Object System.Security.Principal.SecurityIdentifier($InputObject, 0)
-        }
+            $sid = New-Object System.Security.Principal.SecurityIdentifier($InputObject, 0)
+        }
         else {
             Write-Error -Message ("The InputObject of type '{0}' cannot be converted." -f $InputObject.GetType())
         }
+
+        $eap = $ErrorActionPreference 
+        $ErrorActionPreference = "Stop"
+        try {
+            $nTAccountName = $sid.Translate([System.Security.Principal.NTAccount]).Value
+        }
+        catch {
+            if ($a_name.length -eq 0) {
+                $nTAccountName = $sid.Value
+            }
+            else {
+                $nTAccountName = $a_name
+            }
+        }
+        $ErrorActionPreference = $eap        
 
         [byte[]]$bytes = New-Object 'byte[]' $sid.BinaryLength 
         $sid.GetBinaryForm($bytes, 0)
 
         New-Object -TypeName psobject -Property @{
             Sid = $sid
-            NTAccountName = $sid.Translate([System.Security.Principal.NTAccount]).value
+            NTAccountName = $nTAccountName 
             ByteArray = $bytes
             HexArray = $bytes | ForEach-Object { ("{0:x2}" -f $_) }
             LDAPString = ($bytes | ForEach-Object { ("\{0:x2}" -f $_) }) -join("")
